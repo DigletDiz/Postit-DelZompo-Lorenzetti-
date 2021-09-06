2fd4d6652a8cd7dd94f7b1a4fc90bd71b2389de0
DigletDiz <evvaiuffa@hotmail.it>
2021-09-06 22:10:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-zx5s5u-xehyi9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-zx5s5u-xehyi9)
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { DataBaseService } from '../database.service';
+
+import { postIt } from '../app.component';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  constructor(private dbs: DataBaseService) {}
+
+  //@Input() logpostits: Array<postIt>;
+  //@Input() logfavoritesPostits: Array<postIt>;
+  logpostits: Array<postIt> = [];
+  logfavoritesPostits: Array<postIt> = [];
+
+  @Output() eventPosts = new EventEmitter<Array<postIt>>();
+  @Output() eventFavPosts = new EventEmitter<Array<postIt>>();
+  @Output() eventLoggedin = new EventEmitter<string>();
+
+  sendPosts() {
+    this.eventPosts.emit(this.logpostits);
+    this.eventFavPosts.emit(this.logfavoritesPostits);
+    this.eventLoggedin.emit(this.dbs.key);
+  }
+
+  generateNewKey() {
+    this.dbs
+      .generateKey()
+      .subscribe(
+        (x: any) => this.login(x),
+        err => console.error('Observer got an error: ' + err)
+      );
+  }
+
+  login(key: string) {
+    console.log(this.logpostits);
+    this.dbs.key = key;
+    this.dbs.getData().subscribe(
+      (x: any) => {
+        var obj = JSON.parse(x);
+        this.logpostits = obj.postits;
+        if (obj.postits == undefined) {
+          this.logpostits = [];
+          this.uploadPostIts();
+        }
+        this.logfavoritesPostits = this.logpostits.filter(x => x.favourite);
+        this.sendPosts();
+        //console.log(this.logpostits);
+      },
+      err => {
+        console.log('Login fallito');
+        console.error('Observer got an error: ' + err);
+      }
+    );
+  }
+
+  uploadPostIts() {
+    var obj = {
+      postits: this.logpostits
+    };
+    this.dbs.putData(obj).subscribe(
+      (x: any) => {
+        console.log('success');
+      },
+      err => console.error('Observer got an error: ' + err)
+    );
+  }
+
+  ngOnInit() {}
+}
